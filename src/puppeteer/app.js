6abcb49df75adea7228efa1f99618fe0a2a8375d
patrice012 <patricedev02@gmail.com>
2024-01-07 19:56:34 +0000

    Update: Refactoring code base

---
+++ src/puppeteer/app.js
@@ -0,0 +1,63 @@
+// env variable
+const { BASE_URL, broswerView } = require("../../config");
+// puppeteer
+const puppeteer = require("puppeteer");
+// helper functions
+const processLinksSequentially = require("./processLinks");
+const loginProcess = require("../auth/auth");
+// utils
+const { _format } = require("../../utils/formatPDFName");
+const { loadCookies, saveCookies } = require("../../utils/cookies");
+
+const startJob = async () => {
+  const browser = await puppeteer.launch(broswerView);
+  const url = new URL(BASE_URL);
+  const page = await browser.newPage();
+
+  // authentification process
+  try {
+    //get saved cookies
+    const cookies = await loadCookies();
+
+    console.log(cookies, "cookies");
+
+    if (Object.keys(cookies).length === 0 || cookies === "") {
+      await page.goto(url, { timeout: 0 });
+      await loginProcess(page);
+      await saveCookies(await page.cookies());
+    } else {
+      // use saved cookies
+      await page.setCookie(...cookies);
+      await page.goto(url, { timeout: 0 });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  // open modal view
+  await page.waitForSelector('a[data-target="#period_scores_modal_1"]', {
+    visible: true,
+  });
+  const modalBox = await page.$('a[data-target="#period_scores_modal_1"]');
+  await modalBox.click();
+
+  // get all links in popup
+  await page.waitForSelector('table[class="table"] a', {
+    visible: true,
+  });
+  const links = await page.$$eval('table[class="table"] a', (links) =>
+    links.map((link) => link.href)
+  );
+
+  try {
+    /* expose custom function in DOM */
+    await page.exposeFunction("_format", _format);
+    /* process each links */
+    await processLinksSequentially(links, browser);
+  } catch (error) {
+    console.log(error);
+  }
+  await browser.close();
+};
+
+startJob();
